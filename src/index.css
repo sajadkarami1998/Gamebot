c6a61e0606f6253f5ecd4f4f9df6ec3f03926973
sajadkarami1998 <sajadkarami1998@gmail.com>
2025-11-28 05:26:09 +0330

    Create index.css

---
+++ src/index.css
@@ -0,0 +1,9 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+    background-color: #0f172a; /* رنگ پس‌زمینه تیره */
+    margin: 0;
+    font-family: system-ui, -apple-system, sans-serif;
+}
